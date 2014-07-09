6745e44e9d57bab4ed91619fd8e84d3a2370ddee
David Burson <Daverburson@gmail.com>
2014-07-09 20:40:02 +0100

    Implemented list view for posts

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -88,24 +88,24 @@ class AdminTest(LiveServerTestCase):
         self.assertEquals(response.status_code, 200)
 
         #Create the new post
-	  response = self.client.post('/admin/blogengine/post/add/', {
+        response = self.client.post('/admin/blogengine/post/add/', {
 		'title': 'My first post',
 		'text': 'This is my first post',
 		'pub_date_0': '2013-12-28',
 		'pub_date_1': '22:00:04'
 	 },
          follow=True
-          )
-         self.assertEquals(response.status_code, 200)
+         )
+        self.assertEquals(response.status_code, 200)
 
-         # Check added successfully
-         self.assertTrue('added successfully' in response.content)
+        # Check added successfully
+        self.assertTrue('added successfully' in response.content)
 
-         # Check new post now in database
-         all_posts = Post.objects.all()
-         self.assertEquals(len(all_posts), 1)
+        # Check new post now in database
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
 
-     def test_edit_post(self):
+        def test_edit_post(self):
 	    # Create the post
 	    post = Post()
 	    post.title = 'My first post'
@@ -164,3 +164,34 @@ class AdminTest(LiveServerTestCase):
 	    # Check post amended
 	    all_posts = Post.objects.all()
 	    self.assertEquals(len(all_posts), 0)
+
+class PostViewTest(LiveServerTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        # Create the post
+        post = Post()
+        post.title = 'My first post'
+        post.text = 'This is my first blog post'
+        post.pub_date = timezone.now()
+        post.save()
+
+        # Check new post saved
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+
+        # Fetch the index
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+        # Check the post title is in the response
+        self.assertTrue(post.title in response.content)
+
+        # Check the post text is in the response
+        self.assertTrue(post.text in response.content)
+
+        # Check the post date is in the response
+        self.assertTrue(str(post.pub_date.year) in response.content)
+        self.assertTrue(post.pub_date.strftime('%b') in response.content)
+        self.assertTrue(str(post.pub_date.day) in response.content)
